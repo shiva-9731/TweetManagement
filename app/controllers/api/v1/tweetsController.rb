7b085947508a2b6ae426972fb95b491e7f6333dc
Shivakumara M <shivasorab@gmail.com>
2020-06-09 22:13:53 +0530

    tweet curd operations

--- app/controllers/api/v1/tweetsController.rb
+++ app/controllers/api/v1/tweetsController.rb
@@ -1,4 +1,5 @@
 class Api::V1::TweetsController < ApplicationController 
+	before_action :tweet_params, only: [:create]
 	def index
 		authenticate_user
 		if @current_user
@@ -9,5 +10,34 @@ class Api::V1::TweetsController < ApplicationController
 				render json: {message: 'User has no tweets'}, status: :not_found
 			end
 		end
+	end
+
+	def create
+		authenticate_user
+		puts "current_user #{@current_user.inspect}"
+		if !@current_user.nil?
+			tweet = @current_user.tweets.create(tweet_params)
+			render json: {message: 'Tweet created successfully', tweet: tweet}, status: :created
+		end		
+	end
+
+	def destroy
+		authenticate_user
+		if !@current_user.nil?		
+			begin
+				tweet = @current_user.tweets.find params['id']
+				if tweet && tweet.destroy
+					render json: {message: 'Tweet Deleted successfully'}, status: :ok
+				end
+			rescue 
+				render json: {message: 'Invalid tweet id'}, status: :not_found
+			end
+		end
+	end
+
+	private
+	
+	def tweet_params
+		params.require(:tweet).permit(:description)
 	end
 end
